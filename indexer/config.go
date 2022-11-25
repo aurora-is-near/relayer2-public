893c94034fb69af15d1710cc015ae71461416802
Tolga Coplu <tolga.coplu@aurora.dev>
2022-11-25 21:46:54 +0300

    25: Config file organization and CLI Version command improvements

--- indexer/config.go
+++ indexer/config.go
@@ -1,7 +1,9 @@
 package indexer
 
 import (
+	"aurora-relayer-go-common/cmd"
 	"aurora-relayer-go-common/log"
+
 	"github.com/spf13/viper"
 )
 
@@ -45,6 +47,7 @@ func GetConfig() *Config {
 	config := defaultConfig()
 	sub := viper.Sub(configPath)
 	if sub != nil {
+		cmd.BindSubViper(sub, configPath)
 		if err := sub.Unmarshal(&config); err != nil {
 			log.Log().Warn().Err(err).Msgf("failed to parse configuration [%s] from [%s], "+
 				"falling back to defaults", configPath, viper.ConfigFileUsed())
