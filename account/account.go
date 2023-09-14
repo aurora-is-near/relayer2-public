c1789e8f3138c7d1a902d492e93339f4d5412d0e
Altug Keyder <7550161+akeyder@users.noreply.github.com>
2023-09-14 12:29:07 +0400

    separate arch node config

--- account/account.go
+++ account/account.go
@@ -20,9 +20,10 @@ var (
 )
 
 type Account struct {
-	config       *endpoint.EngineConfig
-	nearAccount  *near.Account
-	txnProcessor *TxnProcessor
+	config              *endpoint.EngineConfig
+	nearArchivalAccount *near.Account
+	nearAccount         *near.Account
+	txnProcessor        *TxnProcessor
 }
 
 func NewAccount(config *endpoint.EngineConfig) (*Account, error) {
@@ -30,6 +31,21 @@ func NewAccount(config *endpoint.EngineConfig) (*Account, error) {
 	if config.NearNetworkID == "" || config.NearNodeURL == "" || config.Signer == "" {
 		return nil, fmt.Errorf("one or more Near configuration is missing, please check [nearNetworkID], " +
 			"[NearNodeURL], [signer] under `endpoint->engine` in config file")
+	}
+
+	// Establish engine communication through near archival node and auth the near account
+	nearArchivalCfg := &near.Config{
+		NetworkID:                config.NearNetworkID,
+		NodeURL:                  config.NearArchivalNodeURL,
+		FunctionKeyPrefixPattern: config.FunctionKeyPrefixPattern,
+	}
+	if config.SignerKey != "" {
+		nearArchivalCfg.KeyPath = config.SignerKey
+	}
+	nearReadConn := near.NewConnection(nearArchivalCfg.NodeURL)
+	nearArchivalAccount, err := near.LoadAccount(nearReadConn, nearArchivalCfg, config.Signer)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load Near account from path: [%s]", nearArchivalCfg.KeyPath)
 	}
 
 	// Establish engine communication and auth the near account
@@ -41,6 +57,7 @@ func NewAccount(config *endpoint.EngineConfig) (*Account, error) {
 	if config.SignerKey != "" {
 		nearCfg.KeyPath = config.SignerKey
 	}
+
 	nearConn := near.NewConnection(nearCfg.NodeURL)
 	nearAccount, err := near.LoadAccount(nearConn, nearCfg, config.Signer)
 	if err != nil {
@@ -53,14 +70,24 @@ func NewAccount(config *endpoint.EngineConfig) (*Account, error) {
 	}
 
 	return &Account{
-		config:       config,
-		nearAccount:  nearAccount,
-		txnProcessor: txnProcessor,
+		config:              config,
+		nearAccount:         nearAccount,
+		nearArchivalAccount: nearArchivalAccount,
+		txnProcessor:        txnProcessor,
 	}, nil
 }
 
-func (a *Account) Call(txn []byte, bn *int64) (*string, error) {
-	resp, err := a.nearAccount.ViewFunction(utils.AccountId, "view", txn, bn)
+func (a *Account) Call(txn []byte, bn *int64, fromArchival bool) (*string, error) {
+
+	var resp interface{}
+	var err error
+
+	if fromArchival {
+		resp, err = a.nearArchivalAccount.ViewFunction(utils.AccountId, "view", txn, bn)
+	} else {
+		resp, err = a.nearAccount.ViewFunction(utils.AccountId, "view", txn, bn)
+	}
+
 	if err != nil {
 		return nil, &errs.GenericError{Err: err}
 	}
@@ -75,8 +102,17 @@ func (a *Account) ChainId() (*common.Uint256, error) {
 	return getUint256ResultFromEngineResponse(resp)
 }
 
-func (a *Account) GetBalance(addr []byte, bn *int64) (*common.Uint256, error) {
-	resp, err := a.nearAccount.ViewFunction(utils.AccountId, "get_balance", addr, bn)
+func (a *Account) GetBalance(addr []byte, bn *int64, fromArchival bool) (*common.Uint256, error) {
+
+	var resp interface{}
+	var err error
+
+	if fromArchival {
+		resp, err = a.nearArchivalAccount.ViewFunction(utils.AccountId, "get_balance", addr, bn)
+	} else {
+		resp, err = a.nearAccount.ViewFunction(utils.AccountId, "get_balance", addr, bn)
+	}
+
 	if err != nil {
 		// Return "0x0" for the blocks before Aurora account or before Genesis
 		if strings.Contains(err.Error(), beforeAuroraError) || strings.Contains(err.Error(), beforeGenesisError) {
@@ -87,8 +123,17 @@ func (a *Account) GetBalance(addr []byte, bn *int64) (*common.Uint256, error) {
 	return getUint256ResultFromEngineResponse(resp)
 }
 
-func (a *Account) GetCode(addr []byte, bn *int64) (*string, error) {
-	resp, err := a.nearAccount.ViewFunction(utils.AccountId, "get_code", addr, bn)
+func (a *Account) GetCode(addr []byte, bn *int64, fromArchival bool) (*string, error) {
+
+	var resp interface{}
+	var err error
+
+	if fromArchival {
+		resp, err = a.nearArchivalAccount.ViewFunction(utils.AccountId, "get_code", addr, bn)
+	} else {
+		resp, err = a.nearAccount.ViewFunction(utils.AccountId, "get_code", addr, bn)
+	}
+
 	if err != nil {
 		// Return "0x" for the blocks before Aurora account or before Genesis
 		if strings.Contains(err.Error(), beforeAuroraError) || strings.Contains(err.Error(), beforeGenesisError) {
@@ -99,8 +144,17 @@ func (a *Account) GetCode(addr []byte, bn *int64) (*string, error) {
 	return getStringResultFromEngineResponse(resp)
 }
 
-func (a *Account) GetStorageAt(args []byte, bn *int64) (*string, error) {
-	resp, err := a.nearAccount.ViewFunction(utils.AccountId, "get_storage_at", args, bn)
+func (a *Account) GetStorageAt(args []byte, bn *int64, fromArchival bool) (*string, error) {
+
+	var resp interface{}
+	var err error
+
+	if fromArchival {
+		resp, err = a.nearArchivalAccount.ViewFunction(utils.AccountId, "get_storage_at", args, bn)
+	} else {
+		resp, err = a.nearAccount.ViewFunction(utils.AccountId, "get_storage_at", args, bn)
+	}
+
 	if err != nil {
 		// Return "0x" for the blocks before Aurora account or before Genesis
 		if strings.Contains(err.Error(), beforeAuroraError) || strings.Contains(err.Error(), beforeGenesisError) {
@@ -111,8 +165,17 @@ func (a *Account) GetStorageAt(args []byte, bn *int64) (*string, error) {
 	return getStringResultFromEngineResponse(resp)
 }
 
-func (a *Account) GetTransactionCount(addr []byte, bn *int64) (*common.Uint256, error) {
-	resp, err := a.nearAccount.ViewFunction(utils.AccountId, "get_nonce", addr, bn)
+func (a *Account) GetTransactionCount(addr []byte, bn *int64, fromArchival bool) (*common.Uint256, error) {
+
+	var resp interface{}
+	var err error
+
+	if fromArchival {
+		resp, err = a.nearArchivalAccount.ViewFunction(utils.AccountId, "get_nonce", addr, bn)
+	} else {
+		resp, err = a.nearAccount.ViewFunction(utils.AccountId, "get_nonce", addr, bn)
+	}
+
 	if err != nil {
 		// Return "0x0" for the blocks before Aurora account or before Genesis
 		if strings.Contains(err.Error(), beforeAuroraError) || strings.Contains(err.Error(), beforeGenesisError) {
